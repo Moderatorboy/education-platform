165bcf7196b180d27a51449d4186c6b09e400dd1
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-11-22 16:40:27 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -21,28 +21,141 @@ export const SAMPLE = {
                   video: 'https://rumble.com/embed/v6zuqey/?pub=4no3cq',
                   duration: '01:23:45',
                   date: '20 Nov, 2025'
-                },
-                {
-                  id: 'l2',
-                  title: 'Coordinate Systems And Vectors | Lecture 02 | Vector (Basics)',
-                  video: 'https://rumble.com/embed/v6zur44/?pub=4no3cq',
-                  duration: '01:15:30',
-                  date: '21 Nov, 2025'
                 }
               ],
-              notes: [
-                { id: 'n1', title: 'Algebra Notes', file: '/notes/algebra.pdf' }
-              ],
-              dpp: [
-                { id: 'd1', title: 'Algebra DPP', file: '/dpp/algebra.pdf' }
-              ],
+              notes: [{ id: 'n1', title: 'Algebra Notes', file: '/notes/algebra.pdf' }],
+              dpp: [{ id: 'd1', title: 'Algebra DPP', file: '/dpp/algebra.pdf' }],
               quizzes: [
                 {
                   id: 'q1',
                   title: 'Algebra Quiz',
-                  questions: [
-                    { q: '2+2?', options: ['3', '4', '5'], ans: 1 }
-                  ]
+                  questions: [{ q: '2+2?', options: ['3', '4', '5'], ans: 1 }]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's2',
+          name: 'PHYSICS (JP SIR)',
+          photo: '/jpsir.jpg',
+          chapters: [
+            {
+              id: 'c2',
+              name: 'Atomic Structure',
+              photo: '/ATOMIC.png',
+              lectures: [
+                {
+                  id: 'l2',
+                  title: 'Atomic Structure | Lecture 01 | Basics',
+                  video: 'https://rumble.com/embed/v7abcd/?pub=4no3cq',
+                  duration: '00:45:20',
+                  date: '22 Nov, 2025'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's3',
+          name: 'MATHEMATICS (VJ SIR)',
+          photo: '/vjsir.jpg',
+          chapters: [
+            {
+              id: 'c3',
+              name: 'Trigonometry',
+              photo: '/TRIGO.png',
+              lectures: [
+                {
+                  id: 'l3',
+                  title: 'Trigonometry | Lecture 01 | Basics',
+                  video: 'https://rumble.com/embed/v7efgh/?pub=4no3cq',
+                  duration: '00:50:00',
+                  date: '23 Nov, 2025'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's4',
+          name: 'MATHEMATICS (GB SIR)',
+          photo: '/bgsir.jpg',
+          chapters: [
+            {
+              id: 'c4',
+              name: 'Cell Structure',
+              photo: '/CELL.png',
+              lectures: [
+                {
+                  id: 'l4',
+                  title: 'Cell Structure | Lecture 01 | Basics',
+                  video: 'https://rumble.com/embed/v7ijkl/?pub=4no3cq',
+                  duration: '00:40:00',
+                  date: '24 Nov, 2025'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's5',
+          name: 'ORGANIC (SKM SIR)',
+          photo: '/skmsir.jpg',
+          chapters: [
+            {
+              id: 'c5',
+              name: 'Grammar Basics',
+              photo: '/GRAMMAR.png',
+              lectures: [
+                {
+                  id: 'l5',
+                  title: 'Grammar | Lecture 01 | Nouns',
+                  video: 'https://rumble.com/embed/v7mnop/?pub=4no3cq',
+                  duration: '00:30:00',
+                  date: '25 Nov, 2025'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's6',
+          name: 'INORGANIC (VJ SIR)',
+          photo: '/vjsir.jpg',
+          chapters: [
+            {
+              id: 'c6',
+              name: 'Programming Basics',
+              photo: '/CODE.png',
+              lectures: [
+                {
+                  id: 'l6',
+                  title: 'Programming | Lecture 01 | Intro to C++',
+                  video: 'https://rumble.com/embed/v7qrst/?pub=4no3cq',
+                  duration: '01:00:00',
+                  date: '26 Nov, 2025'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 's7',
+          name: 'PHYSICAL (AKK SIR)',
+          photo: '/akk.jpg',
+          chapters: [
+            {
+              id: 'c7',
+              name: 'Hindi Vyakaran',
+              photo: '/HINDI.png',
+              lectures: [
+                {
+                  id: 'l7',
+                  title: 'Hindi Grammar | Lecture 01 | Sandhi',
+                  video: 'https://rumble.com/embed/v7uvwx/?pub=4no3cq',
+                  duration: '00:35:00',
+                  date: '27 Nov, 2025'
                 }
               ]
             }
