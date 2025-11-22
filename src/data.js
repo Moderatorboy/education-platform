8e1a537b5f6190673591251c66045f88f27e6497
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-11-22 20:55:26 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const SAMPLE = {
           chapters: [
             {
               id: 'ns_c1',
-              name: 'Vectors',
+              name: 'CO-ORDINATE SYSTEM & VECTORS',
               photo: '/VECTOR.png',
               lectures: [
                 { id: 'ns_l1', title: 'Vectors | L-01 | Basics', video: 'https://rumble.com/embed/v1', duration: '01:00:00', date: '20 Nov, 2025' },
@@ -22,7 +22,7 @@ export const SAMPLE = {
             },
             {
               id: 'ns_c2',
-              name: 'Kinematics',
+              name: 'KINEMATICS',
               photo: '/KINEMATICS.png',
               lectures: [
                 { id: 'ns_l4', title: 'Kinematics | L-01 | Motion Basics', video: 'https://rumble.com/embed/v4', duration: '01:00:00', date: '23 Nov, 2025' },
@@ -32,12 +32,102 @@ export const SAMPLE = {
             },
             {
               id: 'ns_c3',
-              name: 'Laws of Motion',
+              name: 'LAWS OF MOTION',
               photo: '/LAWSMOTION.png',
               lectures: [
                 { id: 'ns_l7', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
                 { id: 'ns_l8', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
                 { id: 'ns_l9', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c4',
+              name: 'CIRCULAR MOTION',
+              photo: '/KINEMATICS.png',
+              lectures: [
+                { id: 'ns_l41', title: 'Kinematics | L-01 | Motion Basics', video: 'https://rumble.com/embed/v4', duration: '01:00:00', date: '23 Nov, 2025' },
+                { id: 'ns_l51', title: 'Kinematics | L-02 | Equations of Motion', video: 'https://rumble.com/embed/v5', duration: '01:00:00', date: '24 Nov, 2025' },
+                { id: 'ns_l61', title: 'Kinematics | L-03 | Projectile Motion', video: 'https://rumble.com/embed/v6', duration: '01:00:00', date: '25 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c5',
+              name: 'WORK POWER ENERGY',
+              photo: '/LAWSMOTION.png',
+              lectures: [
+                { id: 'ns_l71', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
+                { id: 'ns_l81', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
+                { id: 'ns_l91', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c6',
+              name: 'CENTRE OF MASS',
+              photo: '/KINEMATICS.png',
+              lectures: [
+                { id: 'ns_l42', title: 'Kinematics | L-01 | Motion Basics', video: 'https://rumble.com/embed/v4', duration: '01:00:00', date: '23 Nov, 2025' },
+                { id: 'ns_l52', title: 'Kinematics | L-02 | Equations of Motion', video: 'https://rumble.com/embed/v5', duration: '01:00:00', date: '24 Nov, 2025' },
+                { id: 'ns_l62', title: 'Kinematics | L-03 | Projectile Motion', video: 'https://rumble.com/embed/v6', duration: '01:00:00', date: '25 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c7',
+              name: 'ROTATIONAL MOTION',
+              photo: '/LAWSMOTION.png',
+              lectures: [
+                { id: 'ns_l72', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
+                { id: 'ns_l82', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
+                { id: 'ns_l92', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c8',
+              name: 'FLUIDS',
+              photo: '/KINEMATICS.png',
+              lectures: [
+                { id: 'ns_l43', title: 'Kinematics | L-01 | Motion Basics', video: 'https://rumble.com/embed/v4', duration: '01:00:00', date: '23 Nov, 2025' },
+                { id: 'ns_l53', title: 'Kinematics | L-02 | Equations of Motion', video: 'https://rumble.com/embed/v5', duration: '01:00:00', date: '24 Nov, 2025' },
+                { id: 'ns_l63', title: 'Kinematics | L-03 | Projectile Motion', video: 'https://rumble.com/embed/v6', duration: '01:00:00', date: '25 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c9',
+              name: 'SHM',
+              photo: '/LAWSMOTION.png',
+              lectures: [
+                { id: 'ns_l73', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
+                { id: 'ns_l83', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
+                { id: 'ns_l93', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c10',
+              name: 'SURFACE TENSION',
+              photo: '/KINEMATICS.png',
+              lectures: [
+                { id: 'ns_l44', title: 'Kinematics | L-01 | Motion Basics', video: 'https://rumble.com/embed/v4', duration: '01:00:00', date: '23 Nov, 2025' },
+                { id: 'ns_l54', title: 'Kinematics | L-02 | Equations of Motion', video: 'https://rumble.com/embed/v5', duration: '01:00:00', date: '24 Nov, 2025' },
+                { id: 'ns_l64', title: 'Kinematics | L-03 | Projectile Motion', video: 'https://rumble.com/embed/v6', duration: '01:00:00', date: '25 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c11',
+              name: 'HEAT AND THERMODYNAMICS',
+              photo: '/LAWSMOTION.png',
+              lectures: [
+                { id: 'ns_l74', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
+                { id: 'ns_l84', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
+                { id: 'ns_l94', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
+              ]
+            },
+            {
+              id: 'ns_c12',
+              name: 'WAVE',
+              photo: '/LAWSMOTION.png',
+              lectures: [
+                { id: 'ns_l75', title: 'Laws of Motion | L-01 | Force Basics', video: 'https://rumble.com/embed/v7', duration: '01:00:00', date: '26 Nov, 2025' },
+                { id: 'ns_l85', title: 'Laws of Motion | L-02 | Newton’s Laws', video: 'https://rumble.com/embed/v8', duration: '01:00:00', date: '27 Nov, 2025' },
+                { id: 'ns_l95', title: 'Laws of Motion | L-03 | Friction', video: 'https://rumble.com/embed/v9', duration: '01:00:00', date: '28 Nov, 2025' }
               ]
             }
           ]
